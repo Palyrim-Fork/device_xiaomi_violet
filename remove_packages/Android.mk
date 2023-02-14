571ea9059bc28212df4fc6a1e479233319f0fed3
kibria5 <106745675+kibria5@users.noreply.github.com>
2023-02-14 04:59:41 +0000

    violet: Nuke more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt PrebuiltDeskClockGoogle Maps Drive PrebuiltGmail Photos YouTube Videos 
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback Aperture talkback
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt PrebuiltDeskClockGoogle Maps Drive PrebuiltGmail Photos YouTube Videos Velvet FilesPrebuilt
+LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback Aperture talkback Chrome Chrome-Stub 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
