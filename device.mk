034a58f48ea46ba2d539ee5fbbe58e3d118cac18
kibria5 <106745675+kibria5@users.noreply.github.com>
2023-03-04 11:00:52 +0000

    violet: OctaviOS Bringup

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2023 The OctaviOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,8 +14,7 @@ $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay 
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
---
+++ octavi_violet.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2023 The OctaviOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
+
+# Inherit some common Octavi stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Sony Dolby
+$(call inherit-product, vendor/dolby/config.mk)
+
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Environment Flags
+TARGET_USES_BLUR := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+OCTAVI_MAINTAINER := kibria5
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := octavi_violet
+PRODUCT_DEVICE := violet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="violet"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
